271188f8a71fe46d318046234b13e348bc712429
Vannucci <homelete@gmail.com>
2023-10-08 22:22:10 -0400

    added class position and tested in program.java

--- src/application/program.java
+++ src/application/program.java
@@ -1,11 +1,17 @@
 package application;
 
+import boardgame.Position;
+
 public class program {
 
 	public static void main(String[] args) {
 
 
 		System.out.println("test");
+		
+		Position position = new Position(3, 4);
+		
+		System.out.println(position);
 
 	}
 
